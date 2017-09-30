863c526c12e69e396b8a9150df24ab2377c1d201
silfo <a143050@gmail.com>
2017-09-30 15:02:38 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in=new Scanner(System.in);
+        int a;
+        int b;
+        a=in.nextInt();
+        b=in.nextInt();
+        System.out.println(a/2.54);
+        System.out.println(b/0.454);
+
+    }
+}
